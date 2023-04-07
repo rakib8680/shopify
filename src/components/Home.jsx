550219a20561352dbce3a7addc1d6cdfe5198d06
rakib8680 <rakib.cst.1st@gmail.com>
2023-04-07 15:01:38 +0600

    data loaded

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import TShirt from './TShirt';
 
 const Home = () => {
 
     const tShirts = useLoaderData();
-    
 
     return (
         <div>
             <h1>Total T-Shirts : {tShirts.length}</h1>
+            {
+                tShirts.map(tshirt => <TShirt tshirt={tshirt} key={tshirt._id}></TShirt>)
+            }
         </div>
     );
 };
---
+++ src/components/TShirt.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TShirt = ({ tshirt }) => {
+    const { name, picture, _id, price, gender } = tshirt
+
+
+    console.log(tshirt)
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default TShirt;
